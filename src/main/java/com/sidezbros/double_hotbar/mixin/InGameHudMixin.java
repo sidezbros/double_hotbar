6f069c8bf823a8a42a315c1fe9921fdedbbe3b4e
sidezbros <ts.sides@gmail.com>
2023-07-18 19:37:58 -0500

    Revert "Convert to DrawContext"
    This reverts commit 105b5bfdd09e71e3b8e7c90cc1d58e38cf59c4cf.

--- src/main/java/com/sidezbros/double_hotbar/mixin/InGameHudMixin.java
+++ src/main/java/com/sidezbros/double_hotbar/mixin/InGameHudMixin.java
@@ -8,55 +8,54 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.sidezbros.double_hotbar.DHModConfig;
 
-import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
 
 @Mixin(InGameHud.class)
-public abstract class InGameHudMixin{
+public abstract class InGameHudMixin extends DrawableHelper{
 
 	private boolean onScreen = false;
 	@Shadow private int scaledHeight;
 	@Shadow private int scaledWidth;
-	@Shadow private static Identifier WIDGETS_TEXTURE;
 	@Shadow protected abstract PlayerEntity getCameraPlayer();
-	@Shadow protected abstract void renderHotbarItem(DrawContext context, int x, int y, float tickDelta, PlayerEntity player, ItemStack stack, int seed);
+	@Shadow protected abstract void renderHotbarItem(MatrixStack matrixStack, int x, int y, float tickDelta, PlayerEntity player, ItemStack stack, int seed);
 	
-	@Inject(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V", ordinal = 0))
-	private void renderHotbarFrame(float tickDelta, DrawContext context, CallbackInfo info) {
+	@Inject(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V", ordinal = 0))
+	private void renderHotbarFrame(float tickDelta, MatrixStack matrices, CallbackInfo info) {
 		if(DHModConfig.INSTANCE.displayDoubleHotbar && !DHModConfig.INSTANCE.disableMod) {
-			context.drawTexture(WIDGETS_TEXTURE, this.scaledWidth / 2 - 91, this.scaledHeight - 22 - DHModConfig.INSTANCE.shift, 0, 0, 182, 22-DHModConfig.INSTANCE.renderCrop);
+			drawTexture(matrices, this.scaledWidth / 2 - 91, this.scaledHeight - 22 - DHModConfig.INSTANCE.shift, 0, 0, 182, 22-DHModConfig.INSTANCE.renderCrop);
 			this.onScreen = true;
 		}
 		
 	}
 	
-	@Inject(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V", ordinal = 1))
-	private void shiftHotbarSelector(float tickDelta, DrawContext context, CallbackInfo info) {
+	@Inject(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V", ordinal = 1))
+	private void shiftHotbarSelector(float tickDelta, MatrixStack matrices, CallbackInfo info) {
 		if(DHModConfig.INSTANCE.displayDoubleHotbar && DHModConfig.INSTANCE.reverseBars && !DHModConfig.INSTANCE.disableMod) {
-			context.getMatrices().translate(0, -DHModConfig.INSTANCE.shift, 0);
+			matrices.translate(0, -DHModConfig.INSTANCE.shift, 0);
 		}
 		
 	}
 	
 	@Inject(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isEmpty()Z", ordinal = 0))
-	private void returnHotbarSelector(float tickDelta, DrawContext context, CallbackInfo info) {
+	private void returnHotbarSelector(float tickDelta, MatrixStack matrices, CallbackInfo info) {
 		if(DHModConfig.INSTANCE.displayDoubleHotbar && DHModConfig.INSTANCE.reverseBars && !DHModConfig.INSTANCE.disableMod) {
-			context.getMatrices().translate(0, DHModConfig.INSTANCE.shift, 0);
+			matrices.translate(0, DHModConfig.INSTANCE.shift, 0);
 		}
 	}
 	
 	@Inject(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;pop()V"))
-	private void shiftHotbarItems(float tickDelta, DrawContext context, CallbackInfo info) {
+	private void shiftHotbarItems(float tickDelta, MatrixStack matrices, CallbackInfo info) {
 		if(DHModConfig.INSTANCE.displayDoubleHotbar && DHModConfig.INSTANCE.reverseBars && !DHModConfig.INSTANCE.disableMod) {
 			this.scaledHeight -= DHModConfig.INSTANCE.shift;
 		}
 	}
 	
 	@Inject(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isEmpty()Z", ordinal = 1))
-	private void renderHotbarItems(float tickDelta, DrawContext context, CallbackInfo info) {
+	private void renderHotbarItems(float tickDelta, MatrixStack matrices, CallbackInfo info) {
 		if(DHModConfig.INSTANCE.displayDoubleHotbar && !DHModConfig.INSTANCE.disableMod) {
 			if(DHModConfig.INSTANCE.reverseBars) {
 				this.scaledHeight += DHModConfig.INSTANCE.shift;
@@ -65,22 +64,22 @@ public abstract class InGameHudMixin{
 			for (int n2 = 0; n2 < 9; ++n2) {
 		            int o = this.scaledWidth / 2 - 90 + n2 * 20 + 2;
 		            int p = this.scaledHeight - 16 - 3 - (DHModConfig.INSTANCE.reverseBars ? 0 : DHModConfig.INSTANCE.shift);
-		            this.renderHotbarItem(context, o, p, tickDelta, getCameraPlayer(), getCameraPlayer().getInventory().main.get(n2+DHModConfig.INSTANCE.inventoryRow*9), m++);
+		            this.renderHotbarItem(matrices, o, p, tickDelta, getCameraPlayer(), getCameraPlayer().getInventory().main.get(n2+DHModConfig.INSTANCE.inventoryRow*9), m++);
 		    }
 		}
 	}
 	
 	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerInteractionManager;hasStatusBars()Z"))
-	public void shiftStatusBars(DrawContext context, float tickDelta, CallbackInfo info) {
+	public void shiftStatusBars(MatrixStack matrices, float tickDelta, CallbackInfo info) {
 		if(this.onScreen) {
-			context.getMatrices().translate(0, -DHModConfig.INSTANCE.shift, 0);
+			matrices.translate(0, -DHModConfig.INSTANCE.shift, 0);
 		}
 	}
 	
 	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;getSleepTimer()I", ordinal = 0))
-	public void returnStatusBars(DrawContext context, float tickDelta, CallbackInfo info) {
+	public void returnStatusBars(MatrixStack matrices, float tickDelta, CallbackInfo info) {
 		if(this.onScreen) {
-			context.getMatrices().translate(0, DHModConfig.INSTANCE.shift, 0);
+			matrices.translate(0, DHModConfig.INSTANCE.shift, 0);
 		}
 		this.onScreen = false;
 	}
