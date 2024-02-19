9674bdfd1b6baf112d549a6804b9c486cc6078b9
sidezbros <ts.sides@gmail.com>
2024-02-19 09:38:05 -0600

    Rare bug fix
    There was a rare bug when loading the player list where there would be nothing returned. Added error handling.

--- src/main/java/com/sidezbros/double_hotbar/mixin/AbstractClientPlayerEntityMixin.java
+++ src/main/java/com/sidezbros/double_hotbar/mixin/AbstractClientPlayerEntityMixin.java
@@ -18,14 +18,18 @@ public abstract class AbstractClientPlayerEntityMixin {
 	
 	@Shadow protected abstract PlayerListEntry getPlayerListEntry();
 	
-	@Inject(method = "getSkinTextures", at = @At("RETURN"), cancellable = true)//
-	private void getSkinTextures(CallbackInfoReturnable<SkinTextures> cir) {//
-		PlayerListEntry playerListEntry = this.getPlayerListEntry();
-		if(playerListEntry.getProfile().getId().toString().equals("f2d832c6-c3b4-41ed-937e-f49cd71c98a7")) {
-			SkinTextures skin_texture = playerListEntry.getSkinTextures();
-			Identifier elytraTexture = new Identifier("double_hotbar", "textures/elytra.png");
-			SkinTextures texture = new SkinTextures(skin_texture.texture(), skin_texture.textureUrl(), elytraTexture, elytraTexture, skin_texture.model(), skin_texture.secure());
-			cir.setReturnValue(texture);
+	@Inject(method = "getSkinTextures", at = @At("RETURN"), cancellable = true)
+	private void getSkinTextures(CallbackInfoReturnable<SkinTextures> cir) {
+		try {
+			PlayerListEntry playerListEntry = this.getPlayerListEntry();
+			if(playerListEntry.getProfile().getId().toString().equals("f2d832c6-c3b4-41ed-937e-f49cd71c98a7")) {
+				SkinTextures skin_texture = playerListEntry.getSkinTextures();
+				Identifier elytraTexture = new Identifier("double_hotbar", "textures/elytra.png");
+				SkinTextures texture = new SkinTextures(skin_texture.texture(), skin_texture.textureUrl(), elytraTexture, elytraTexture, skin_texture.model(), skin_texture.secure());
+				cir.setReturnValue(texture);
+			}
+		} catch(Exception e) {
+			// If playerListEntry fails, ignore and move on.
 		}
 	}
 }
